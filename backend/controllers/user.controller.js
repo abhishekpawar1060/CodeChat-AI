5ceb4f6799abbb9a16cbe0ad3ca88a25fb02136e
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-12-29 17:18:49 +0530

    add middleware for authuser

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -55,3 +55,11 @@ export const loginController = async(req, res) => {
         res.status(400).send(error.message);
     }
 }
+
+
+export const profileController = async (req, res) => {
+    console.log(res.user);
+    res.status(200).json({
+        user: req.user
+    });
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import * as userController from '../controllers/user.controller.js';
 import { body } from 'express-validator';
-
+import * as authMiddleware from '../middleware/auth.middleware.js';
 
 const router = Router();
 
@@ -15,5 +15,20 @@ router.post('/login',
     body('email').isEmail().withMessage('Email must be valid email address'),
     body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 character long'),
     userController.loginController
-)
+);
+
+
+router.get('/profile', 
+    authMiddleware.authUser,
+    userController.profileController
+);
+
+
+
+
+
+
+
+
+
 export default router;
