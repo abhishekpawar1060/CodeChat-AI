42774e0478192ec2c09dc3bdc5a3aba92890f1e6
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2025-01-13 19:39:15 +0530

    integrate project component with backend

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,36 +1,60 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import axios from '../config/axios';
 
 function Project() {
 
     const location = useLocation();
-    console.log(location.state);
+    // console.log(location.state);
 
     const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedUserId, setSelectedUserId] = useState([]);
 
-    const user = [
-        {id: 1, name: 'John1  jfklwhdasshknnkjnjpaw'},
-        {id: 2, name: 'John2'},
-        {id: 3, name: 'John3'},
-        {id: 4, name: 'John1  jfklwhdassjkjhknnkjnjpaw'},
-        {id: 5, name: 'John2'},
-        {id: 6, name: 'John3'},
-        {id: 7, name: 'John1  jfklwhdasshknnkjnjpaw'},
-        {id: 8, name: 'John2'},
-        {id: 10, name: 'John3'},
-        {id: 11, name: 'John1  jfklwhdasshknnkjnjpaw'},
-        {id: 22, name: 'John2'},
-        {id: 33, name: 'John3'},
-        {id: 12, name: 'John1  jfklwhdasshknnkjnjpaw'},
-        {id: 54, name: 'John2'},
-        
-    ]
-    
+    const [users, setUsers] = useState([]);
+    const [project, setProject] = useState(location.state.project);
+
     const handleUserClick = (id) => {
-        setSelectedUserId([...selectedUserId, id]);
+        setSelectedUserId(prevSelectedUserId => {
+            const newSelectedUserId = new Set(prevSelectedUserId);
+            if(newSelectedUserId.has(id)){
+                newSelectedUserId.delete(id);
+            }else{
+                newSelectedUserId.add(id);
+            }            
+            return newSelectedUserId;
+        });
     }
+
+
+    function addCollaborators(){
+        axios.put('/projects/add-user', {
+            projectId: location.state.project._id,
+            users: Array.from(selectedUserId)
+        }).then((res) => {
+            console.log(res.data);
+            setIsModalOpen(false);
+        }).catch((err) => {
+            console.log(err);
+        })   
+    }
+
+    useEffect(() => {
+        
+        axios.get(`/projects/get-project/${location.state.project._id}`).then((res) => {
+            setProject(res.data.project);
+            console.log("Project",project);
+        }).catch((err) => {
+            console.log(err);
+        })
+
+
+        axios.get('/users/all').then((res) => {
+            setUsers(res.data.users);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, []);
     
     
     return (
@@ -79,26 +103,29 @@ function Project() {
                 </div>
 
                 <div className={`sidePanel flex flex-col gap-2 w-full h-full bg-slate-50 absolute transition-all ${isSidePanelOpen ? 'translate-x-0' : '-translate-x-full'} top-0`}> 
-                    <header className='flex justify-end px-3 bg-slate-200'>
+                    <header className='flex justify-between items-center px-4 bg-slate-200'>
+                        <h1 className='font-semibold text-lg'>Collaborators</h1>
                         <button
                             onClick={() => setIsSidePanelOpen(!isSidePanelOpen)}    
                             className='p-2'
                         >
-                            <i className='ri-close-line'></i>
+                            <i className='ri-close-line text-xl text-red-500 hover:bg-red-300'></i>
                         </button>
                     </header>
 
                     <div className="users flex flex-col gap-2">
-                        <div className="user cursor-pointer flex gap-2 items-center hover:bg-slate-200 p-2">
-                            <div className='aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
-                                <i className="ri-user-fill absolute"></i>
-                            </div>
-
-                            <h1 className='font-semibold text-lg'>
-                                userName
-                            </h1>
-
-                        </div>
+                        {project.users && project.users.map((user) => {
+                            return (
+                                <div key={user._id} className="user cursor-pointer flex gap-2 items-center hover:bg-slate-200 p-2">
+                                    <div className='aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
+                                        <i className="ri-user-fill absolute"></i>
+                                    </div>
+                                    <h1 className='font-semibold text-lg'>
+                                        {user.email}
+                                    </h1>
+                                </div>
+                            );
+                        })}
                     </div> 
                 </div>
             </section>
@@ -113,23 +140,26 @@ function Project() {
                             </button>
                         </header>
                         <div className='users-list flex flex-col gap-2 mb-16 max-h-96 overflow-auto'>
-                            {user.map((user) => (
+                            {users.map((user) => (
                                 <div 
-                                    key={user.id}
-                                    onClick={() => handleUserClick(user.id)}
-                                    className={`user cursor-pointer flex gap-2 items-center hover:bg-slate-200 ${selectedUserId.indexOf(user.id) != -1 ? 'bg-slate-200' : ''} p-2`}
+                                    key={user._id}
+                                    onClick={() => handleUserClick(user._id)}
+                                    className={`user cursor-pointer flex gap-2 items-center hover:bg-slate-300 ${Array.from(selectedUserId).indexOf(user._id) != -1 ? 'bg-slate-400' : ''} p-2`}
                                 >
                                     <div className='aspect-square relative rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
                                         <i className="ri-user-fill absolute"></i>
                                     </div>
 
                                     <h1 className='font-semibold text-lg'>
-                                        {user.name}
+                                        {user.email}
                                     </h1>
                                 </div>
                             ))}
                         </div>
-                        <button className='relative bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-blue-500 text-white rounded-md'>
+                        <button
+                            onClick={addCollaborators}
+                            className='relative bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-blue-500 text-white rounded-md'
+                        >
                             Add Collaborators
                         </button>
                     </div>
