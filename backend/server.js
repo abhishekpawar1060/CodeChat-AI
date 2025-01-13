362fa582055ce2f9e70544780dfebf124868a4f8
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2025-01-13 22:25:04 +0530

    middleware for socket.io

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,36 @@ import 'dotenv/config';
 import http from 'http';
 import {Server} from 'socket.io';
 import app from './app.js';
-
+import jwt from 'jsonwebtoken';
+import { decode } from 'punycode';
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
 const io = new Server(server);
+
+io.use((socket, next) => {
+    try {
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization.split(' ')[1];
+        if(!token) {
+            return next(new Error('Authentication error'));
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded) {
+            return next(new Error('Authentication error'));
+        }
+
+        socket.decoded = decoded;
+
+        next();
+
+    } catch (error) {
+        next(new Error('Authentication error'));
+    }
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected:', socket.id);
